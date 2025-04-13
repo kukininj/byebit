5f89638f08b8d60f938d5b5f2b6966e32dbba9ac
lukasz <luk20001122@gmail.com>
2025-04-13 20:42:13 +0200

    budowanie kontraktów

--- contracts/src/main/solidity/HelloWorld.sol
+++ contracts/src/main/solidity/HelloWorld.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20; // Specifies the compiler version
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.7.0;
 
 /**
  * @title HelloWorld
---
+++ contracts/src/test/java/org/web3j/generated/contracts/HelloWorldTest.java
@@ -0,0 +1,31 @@
+package org.web3j.generated.contracts;
+
+import java.lang.Exception;
+import java.lang.String;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.web3j.EVMTest;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+@EVMTest
+class HelloWorldTest {
+    private static HelloWorld helloWorld;
+
+    @BeforeAll
+    static void deploy(Web3j web3j, TransactionManager transactionManager,
+                       ContractGasProvider contractGasProvider) throws Exception {
+        helloWorld = HelloWorld.deploy(web3j, transactionManager, contractGasProvider).send();
+    }
+
+    @Test
+    public void greeting() throws Exception {
+    String stringVar = helloWorld.sayHello().send();
+    Assertions.assertEquals("Hello World!", stringVar);
+    }
+
+}
