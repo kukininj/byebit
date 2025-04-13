cdb0dd61c6e9987b50cfa780eb684ab49bd7b426
lukasz <luk20001122@gmail.com>
2025-04-13 21:39:59 +0200

    test pobierania stanu konta

--- app/src/test/java/com/example/byebit/web3j/SepoliaConnectionWorks.java
+++ app/src/test/java/com/example/byebit/web3j/SepoliaConnectionWorks.java
@@ -3,8 +3,10 @@ package com.example.byebit.web3j;
 import org.junit.Assert;
 import org.web3j.protocol.Web3j;
 import org.junit.Test;
+import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.http.HttpService;
 
 import java.math.BigInteger;
@@ -14,14 +16,34 @@ public class SepoliaConnectionWorks {
     public void connectionWorks() {
         Web3j web3j = Web3j.build(new HttpService("https://sepolia.drpc.org"));
 
-        Request<?, EthBlockNumber> ethBlockNumberRequest = web3j.ethBlockNumber();
-
         try {
+            Request<?, EthBlockNumber> ethBlockNumberRequest = web3j.ethBlockNumber();
+
             BigInteger blockNumber = ethBlockNumberRequest.send().getBlockNumber();
 
             System.out.println(blockNumber);
 
             Assert.assertTrue(blockNumber.compareTo(BigInteger.ONE) > 0);
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void checkBalanceTest() {
+        Web3j web3j = Web3j.build(new HttpService("https://sepolia.drpc.org"));
+
+        try {
+            Request<?, EthGetBalance> ethGetBalanceRequest = web3j.ethGetBalance(
+                    "0x9d1E794bd663C2050096b0A0C7f8c9a4de9759C7",
+                    DefaultBlockParameter.valueOf("latest")
+            );
+
+            BigInteger balance = ethGetBalanceRequest.send().getBalance();
+
+            System.out.println(balance);
+
+            Assert.assertTrue(balance.compareTo(BigInteger.ZERO) > 0);
         } catch (Exception e) {
             Assert.fail(e.getMessage());
         }
